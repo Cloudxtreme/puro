41730cd7a61fa3fba7bc958ca3220ae73777e457
Piotr Zduniak <piotr@zduniak.net>
2015-04-08 01:13:00 +0200

    Safer SSL settings

--- main.go
+++ main.go
@@ -57,6 +57,17 @@ func main() {
 	// Create a new TLS config
 	tlsConfig = &tls.Config{
 		Certificates: certs,
+		MinVersion:   tls.VersionTLS10,
+		CipherSuites: []uint16{
+			tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+		},
 	}
 	tlsConfig.BuildNameToCertificate()
 

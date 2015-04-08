6a3438cac90b1f85ecd43c0227c3d0b3253ae849
Piotr Zduniak <piotr@zduniak.net>
2015-04-08 18:33:43 +0200

    Proper TLS fallback fix

--- main.go
+++ main.go
@@ -58,6 +58,7 @@ func main() {
 	tlsConfig = &tls.Config{
 		Certificates: certs,
 		MinVersion:   tls.VersionTLS10,
+		MaxVersion:   tls.VersionTLS12,
 		CipherSuites: []uint16{
 			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
@@ -67,7 +68,6 @@ func main() {
 			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
 			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
 			tls.TLS_RSA_WITH_AES_128_CBC_SHA,
-			tls.TLS_FALLBACK_SCSV,
 		},
 		PreferServerCipherSuites: true,
 	}

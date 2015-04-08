34e4d715b71444fbd628de93baaa69eda13d884b
Piotr Zduniak <piotr@zduniak.net>
2015-04-08 18:20:42 +0200

    SCSV TLS fallback

--- main.go
+++ main.go
@@ -67,6 +67,7 @@ func main() {
 			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
 			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
 			tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_FALLBACK_SCSV,
 		},
 		PreferServerCipherSuites: true,
 	}
